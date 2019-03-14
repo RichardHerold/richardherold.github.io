86342e7d112a19c2a64776408f40a71f5c48cbcd
Richard Herold <Richardherold@gmail.com>
2019-03-13 21:54:32 -0400

    V2 start using forty

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Hello, I am Richard</h1>
+            </header>
+            <div className="content">
+                <p>I make things on the internet<br />
+                and have too many hobbies.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
