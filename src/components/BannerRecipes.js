fc6aefb2e60910004f7a323e00c47f3553190db7
Richard Herold <Richardherold@gmail.com>
2019-03-14 13:49:44 -0400

    taught myself that style5 = the accent in vars.scss

--- src/components/BannerRecipes.js
+++ src/components/BannerRecipes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const BannerRecipes = (props) => (
-    <section id="banner" className="style2">
+    <section id="banner" className="style6">
         <div className="inner">
             <header className="major">
                 <h1>Recipes</h1>
