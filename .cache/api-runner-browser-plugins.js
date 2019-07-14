ef21f9f32b28884eb013d3c47e0c1f96e6698ecb
RichardHerold <richardherold@gmail.com>
2019-07-14 17:23:22 -0400

    updated

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,7 +1,7 @@
 module.exports = [{
-      plugin: require('/Users/richardherold/Documents/GitHub/richardherold.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      plugin: require('C:/users/richa/documents/GitHub/richardherold.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/richardherold/Documents/GitHub/richardherold.github.io/gatsby-browser.js'),
+      plugin: require('C:/users/richa/documents/GitHub/richardherold.github.io/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,9 +1,12 @@
 var plugins = [{
-      plugin: require('/Users/richardherold/Documents/GitHub/richardherold.github.io/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('C:/users/richa/documents/GitHub/richardherold.github.io/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/richardherold/Documents/GitHub/richardherold.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('C:/users/richa/documents/GitHub/richardherold.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/assets/images/website-icon.png"},
+    },{
+      plugin: require('C:/users/richa/documents/GitHub/richardherold.github.io/node_modules/gatsby-plugin-offline/gatsby-ssr'),
+      options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
